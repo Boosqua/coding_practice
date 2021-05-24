93fb0e59ec5ec8a70162ce463616203d9bec9e6a
Omar Hernandez <oah232@gmail.com>
2021-05-23 17:49:42 -0700

    added WIP BST

---
+++ testing.js
@@ -0,0 +1,93 @@
+// Do not edit the class below except for
+// the insert, contains, and remove methods.
+// Feel free to add new properties and methods
+// to the class.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+		if(this.contains(value)){
+			return this
+		}
+		if( this.value > value ){
+			if( this.left ){
+				this.left.insert(value)
+			} else {
+				this.left = new BST(value)
+			}
+		} else if (this.value < value){
+			if( this.right ){
+				this.right.insert(value)
+			} else {
+				this.right = new BST(value)
+			}
+		}
+    return this;
+  }
+
+  contains(value) {
+    // Write your code here.
+		//Base case 
+		if( value === this.value ){
+			return true
+		} else if( 
+			(this.value > value && !this.left) ||
+			!this.right 
+		){
+			return false
+		} 
+		return this.value < value ? this.right.contains(value) : this.left.contains(value)
+  }
+	find(value){
+		if(!this.contains(value)){
+			return null
+		} 
+		if( this.value === value ){
+			return [this]
+		} else {
+			let nextNode = this.value > value ? this.left.find(value) : this.right.find(value)
+			return [this].concat(nextNode)
+		}
+	}
+  remove( value ) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+		if( //return this if value not in tree or value is rootnode and no leafs
+			!this.contains(value) || 
+			(this.value === value && !this.left && !this.right ) ){
+			return this
+		} 
+		const nodes = this.find(value)
+		const removed = nodes[ nodes.length - 1 ]
+		const parent = nodes[ nodes.length - 2 ]
+		if( removed.right ){
+			const replacementVal = removed.min()
+			this.remove( replacementVal.value )
+			removed.value = replacementVal.value
+		} else {
+			const replacement = removed.left? removed.left : null
+			if( parent.right.value === value ){
+				parent.right = replacement
+			} else {
+				parent.left = replacement
+			}
+		} 
+    return this;
+  }
+	min(){
+		if( !this.left ){
+			return this
+		} else {
+			return this.left.min()
+		}
+	}
+}
+
+// Do not edit the line below.
+exports.BST = BST;
